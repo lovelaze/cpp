de05b4dd9e92c22cda6870242dc37fc507ea64d0
Fredrik Berntsson <fredrik.kona@gmail.com>
2014-10-31 14:15:29 +0100

    test.cpp

---
+++ lab2/test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "julian.h"
+#include "gregorian.h"
+#include "kattistime.h"
+
+int main() {
+
+	time_t tp;
+    time(&tp);    
+    set_k_time(tp);
+
+
+	lab2::Gregorian g;
+	lab2::Julian j;
+
+	std::cout << g << std::endl;
+	g.debug();
+	std::cout << g.mod_julian_day() << std::endl;
+	std::cout << g.week_day() << std::endl;
+	std::cout << g.week_day_name() << std::endl;
+	std::cout << g.month_name() << std::endl;
+	
+	std::cout << std::endl;
+
+	std::cout << j << std::endl;
+	j.debug();
+	std::cout << j.mod_julian_day() << std::endl;
+	g+=2;
+	g.debug();
+
+}
